3445906d12c61de5b5183f1e31a328195bb4768e
Víctor Rolack <victorolack@gmail.com>
2021-04-02 15:13:53 -0300

    New feature: link_scraping() and implemented on sendMessages

--- coins/chainlink.py
+++ coins/chainlink.py
@@ -1,3 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 import schedule
+
+def link_scraping():
+    url = requests.get('https://www.coingecko.com/es/monedas/chainlink')
+    soup = BeautifulSoup(url.content, 'html.parser')
+    result = soup.find('span', {'class': 'no-wrap'})
+    format_result = result.text
+
+    return format_result
--- sendMessages.py
+++ sendMessages.py
@@ -4,6 +4,7 @@ import schedule
 import coins.bitcoin as btc
 import coins.polkadot as dot
 import coins.ethereum as eth
+import coins.chainlink as link
 
 def bot_send_text(bot_message):
     
@@ -19,9 +20,11 @@ def report():
     btc_price = f'The price of Bitcoin today is {btc.btc_scraping()}'
     dot_price = f'The price of Polkadot today is {dot.dot_scraping()}'
     eth_price = f'The price of Ethereum today is {eth.eth_scraping()}'
+    link_price = f'The price of Chainlink today is {link.link_scraping()}'
     bot_send_text(btc_price)
     bot_send_text(dot_price)
     bot_send_text(eth_price)
+    bot_send_text(link_price)
 
 
 if __name__ == '__main__':
