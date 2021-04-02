254141347679bf1f1d9ffc05ddba1f785ba59bc3
Víctor Rolack <victorolack@gmail.com>
2021-04-02 14:39:14 -0300

    refactor: btc_scraping() move to bitcoin.py

---
+++ coins/bitcoin.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+import schedule
+
+def btc_scraping():
+
+    url = requests.get('https://awebanalysis.com/es/coin-details/bitcoin/')
+    soup = BeautifulSoup(url.content, 'html.parser')
+    result = soup.find('td', {'class': 'wbreak_word align-middle coin_price'})
+    format_result = result.text
+
+    return format_result
--- sendMessages.py
+++ sendMessages.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import schedule
-import './coins/bitcoin'
+import coins.bitcoin as btc
 
 def bot_send_text(bot_message):
     
@@ -13,22 +13,13 @@ def bot_send_text(bot_message):
 
     return response
 
-def btc_scraping():
-
-    url = requests.get('https://awebanalysis.com/es/coin-details/bitcoin/')
-    soup = BeautifulSoup(url.content, 'html.parser')
-    result = soup.find('td', {'class': 'wbreak_word align-middle coin_price'})
-    format_result = result.text
-
-    return format_result
-
 def report():
-    btc_price = f'The price of Bitcoin today is {btc_scraping()}'
+    btc_price = f'The price of Bitcoin today is {btc.btc_scraping()}'
     bot_send_text(btc_price)
 
 if __name__ == '__main__':
 
-    schedule.every().day.at("08:00").do(report)
-
+    schedule.every().day.at("14:35").do(report)
+    report()
     while True:
         schedule.run_pending()
