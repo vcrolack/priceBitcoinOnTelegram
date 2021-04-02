357d747a9cb43be0d52d05bf67271ee03fedb014
Víctor Rolack <victorolack@gmail.com>
2021-04-02 15:09:27 -0300

    New feature: eth_scraping() and implemented on sendMessages.py

--- coins/ethereum.py
+++ coins/ethereum.py
@@ -1,3 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import schedule
+
+def eth_scraping():
+
+    url = requests.get('https://www.coingecko.com/es/monedas/ethereum')
+    soup = BeautifulSoup(url.content, 'html.parser')
+    result = soup.find('span', {'class': 'no-wrap'})
+    format_result = result.text
+
+    return format_result
--- sendMessages.py
+++ sendMessages.py
@@ -3,6 +3,7 @@ import requests
 import schedule
 import coins.bitcoin as btc
 import coins.polkadot as dot
+import coins.ethereum as eth
 
 def bot_send_text(bot_message):
     
@@ -17,8 +18,11 @@ def bot_send_text(bot_message):
 def report():
     btc_price = f'The price of Bitcoin today is {btc.btc_scraping()}'
     dot_price = f'The price of Polkadot today is {dot.dot_scraping()}'
+    eth_price = f'The price of Ethereum today is {eth.eth_scraping()}'
     bot_send_text(btc_price)
     bot_send_text(dot_price)
+    bot_send_text(eth_price)
+
 
 if __name__ == '__main__':
 
